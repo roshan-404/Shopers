1e1a3743dcaedd904ae3f4122930bb5e32599902
Roshan Kumar Ojha <roshanboyrko@gmail.com>
2021-06-06 01:13:15 +0530

    Added styles to product card

--- src/components/products/Product/Product.jsx
+++ src/components/products/Product/Product.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons' 
-import classes from '*.module.css'
+
+import useStyles from './styles'
 
 const Product = ({product}) => {
+
+    const classes = useStyles()
+
     return (
         <Card className={classes.root}>
             <CardMedia classname={classes.media} image='' title={product.name} />
